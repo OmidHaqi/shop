9ffb34270ae32ed4825acafd20ed9dfe6b8aad9e
Umut <omid.haqi@outlook.com>
2024-11-26 16:02:08 +0330

    register Dio as a singleton in the service locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shop/common/utils/prefs_operator.dart';
@@ -5,7 +6,11 @@ import 'package:shop/common/utils/prefs_operator.dart';
 GetIt locator = GetIt.instance;
 
 Future<void> initLocator() async {
+
+  locator.registerSingleton<Dio>(Dio());
+
   SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
   locator.registerSingleton(sharedPreferences);
+  
   locator.registerSingleton<PrefsOperator>(PrefsOperator());
 }
